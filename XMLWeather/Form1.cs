0422a16288f9755307876671bc22a03ff68f832a
JackMaca <me4hemi@hotmail.com>
2016-04-26 10:09:11 -0400

    Initial Commit - current weather needs images for different temperatures and the date displayed to be completed.

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -16,54 +16,11 @@ namespace XMLWeather
         public Form1()
         {
             InitializeComponent();
-
-            // get information about current and forecast weather from the internet
-            GetData();
-
-            // take info from the current weather file and display it to the screen
-            ExtractCurrent();
-
-            // take info from the forecast weather file and display it to the screen
-            ExtractForecast();
+            //opens Todays weather
+            currentWeather cw = new currentWeather();
+            this.Controls.Add(cw);
+            cw.Location = new Point((this.Width - cw.Width) / 2, (this.Height - cw.Height) / 2);
+            cw.Show();
         }
-
-        private static void GetData()
-        {
-            WebClient client = new WebClient();
-
-            string currentFile = "http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
-            string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
-            
-            client.DownloadFile(currentFile, "WeatherData.xml");
-            client.DownloadFile(forecastFile, "WeatherData7Day.xml");
-        }
-
-        private void ExtractCurrent()
-        {
-            XmlDocument doc = new XmlDocument();
-            doc.Load("WeatherData.xml");
-
-            //create a node variable to represent the parent element
-            XmlNode parent;
-            parent = doc.DocumentElement;
-
-            //check each child of the parent element
-            foreach (XmlNode child in parent.ChildNodes)
-            {
-                // TODO if the "city" element is found display the value of it's "name" attribute
-            }
-        }
-              
-        private void ExtractForecast()
-        {
-                     
-        }
-
-
-        private void MakePictureParent()
-        {
-            
-        }
-
     }
 }
---
+++ XMLWeather/forecastWeather.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+
+namespace XMLWeather
+{
+    public partial class forecastWeather : UserControl
+    {
+        public forecastWeather()
+        {
+            InitializeComponent();
+
+            // take info from the forecast weather file and display it to the screen
+
+            // get information about current and forecast weather from the internet
+            GetData();
+
+            // take info from the forecast weather file and display it to the screen
+            ExtractForecast();
+        }
+
+        private static void GetData()
+        {
+            WebClient client = new WebClient();
+
+            string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
+
+            //client.DownloadFile(forecastFile, "WeatherData7Day.xml");
+        }
+
+        private void ExtractForecast()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("WeatherData7Day.xml");
+
+            //create a node variable to represent the parent element
+            XmlNode parent;
+            parent = doc.DocumentElement;
+            int day = 1;
+
+            //check each child of the parent element
+            foreach (XmlNode child in parent.ChildNodes)
+            {
+                if (child.Name == "forecast")
+                {
+                    foreach (XmlNode grandChild in child.ChildNodes)
+                    {
+                        foreach (XmlNode greatGrandChild in grandChild.ChildNodes)
+                        {
+                            if (greatGrandChild.Name == "temperature")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        tempLabel1.Text = greatGrandChild.Attributes["day"].Value + "°C";
+                                        break;
+                                    case 2:
+                                        tempLabel2.Text = greatGrandChild.Attributes["day"].Value + "°C";
+                                        break;
+                                    case 3:
+                                        tempLabel3.Text = greatGrandChild.Attributes["day"].Value + "°C";
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            if (greatGrandChild.Name == "precipitation")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        rainLabel1.Text = greatGrandChild.Attributes["type"].Value;
+                                        day++;
+                                        break;
+                                    case 2:
+                                        rainLabel2.Text = greatGrandChild.Attributes["type"].Value;
+                                        day++;
+                                        break;
+                                    case 3:
+                                        rainLabel3.Text = greatGrandChild.Attributes["type"].Value;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            ((Form)this.TopLevelControl).Close();
+        }
+    }
+}
